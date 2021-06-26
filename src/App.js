3a7a273636fcf5eac078e9e402b9fd5bca2920f5
Gurmanjot Singh Randhawa <gurmanjot@geekyants.com>
2021-06-26 18:35:49 +0530

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Sidebar from "./components/SideBar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <div className="bg-blue-500 flex-1"></div>
+      <div className=" flex-1">
+        <Header />
+      </div>
     </div>
   );
 }
